a27a77f44621e5f4d5b0b3a055df678c588d2043
raquelmontilla <rmontillacalvo@gmail>
2019-08-12 13:43:48 +0200

    started exercise

---
+++ _src/assets/js/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const myUrl = 'https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/palette.json';
+
+const getDataFromServer = () => {
+  fetch
+}
+
+
